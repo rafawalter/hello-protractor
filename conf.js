7d20f893692be7a0c584b61a32c9d0f16aafcf14
Rafael Walter <rafawalter@gmail.com>
2016-03-12 15:47:44 -0300

    removendo direct connect

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
   framework: 'jasmine',
-  directConnect: true,
+  //directConnect: true,
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   selelniumPort: 4444,
   specs: ['spec.js'],
