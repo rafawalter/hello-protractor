8472bedb9e6a81e9b0809227683e5766b91d60d2
Rafael Walter <rafawalter@gmail.com>
2016-03-12 15:45:21 -0300

    informar a portal do Selenium ajuda?

--- conf.js
+++ conf.js
@@ -1,7 +1,8 @@
 exports.config = {
   framework: 'jasmine',
   directConnect: true,
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  selelniumPort: 4444,
   specs: ['spec.js'],
   multiCapabilities: [{
     browserName: 'chrome'
