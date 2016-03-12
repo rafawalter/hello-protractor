175fa0a15fe030898eb4fe2ef118c3ef9e83c6b8
Rafael Walter <rafawalter@gmail.com>
2016-03-12 14:27:18 -0300

    add four and six

--- spec.js
+++ spec.js
@@ -1,17 +1,32 @@
 describe('Protractor Demo App', function() {
-  it('should have a title',  function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo');
+  });
+
+  it('should have a title',  function() {
     expect(browser.getTitle()).toEqual('Super Calculator');
   });
 
   it('should add onde and two', function() {
-    browser.get('http://juliemr.github.io/protractor-demo');
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
 
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+    goButton.click();
 
-    element(by.id('gobutton')).click();
+    expect(latestResult.getText()).toEqual('3');
+  });
 
-    expect(element(by.binding('latest')).getText()).toEqual('3');
+  it('should add four and six', function() {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
   });
 });
